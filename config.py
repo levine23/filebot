4e4a522fa60da2bce1a37ff73472c87da32962cb
levine23 <annacantik461@gmail.com>
2025-06-17 07:23:48 +0900

    Update config.py

--- config.py
+++ config.py
@@ -17,9 +17,7 @@ DB_NAME = os.environ.get("DB_NAME", "userbot")
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002783872261"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002841696910"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-100"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
